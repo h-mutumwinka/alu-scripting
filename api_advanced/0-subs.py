af0aca632cdf7d73daf11b117c1da1b558775564
h-mutumwinka <h.mutumwink@alustudent.com>
2025-02-19 17:10:03 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""function that queries the 'Reddit API' and returns the number of subscribers"""
+"""function that queries the 'Reddit API' and returns subscribers"""
 import requests
 
 
